5e221fbf1baeedcf44076ccb49a04a0b72a14c21
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-02-23 17:42:26 -0600

    chore(deps): update eslint
    Remove JSCS and JSHint.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-'use strict';
-import deepEqual from 'deep-equal';
-
-module.exports = {
-  spy
-};
+'use strict'
+import deepEqual from 'deep-equal'
 
 /*
  * Adds spy related utilities to obj[methodName] or creates a new Spied Function
@@ -16,34 +12,33 @@ module.exports = {
  * @param {*} [methodName] - obj[methodName] must be a function and will be spied on
  * @return {Object} - return Spied Function for easy chaining
  */
-function spy(obj, methodName) {
-  const isNewSpy = !arguments.length;
+const spy = function (obj, methodName) {
+  const isNewSpy = !arguments.length
   let spyReturn = {}
     , withArgsReturns = []
-    , originalFn, spiedFn;
 
   if (isNewSpy) {
     /* eslint no-param-reassign: 0 */
     obj = {
       spy() {}
-    };
+    }
 
-    methodName = 'spy';
+    methodName = 'spy'
   }
 
   if (obj === null || obj === undefined) {
-    throw new TypeError('Expected obj to not be null or undefined');
+    throw new TypeError('Expected obj to not be null or undefined')
   }
 
-  originalFn = obj[methodName];
+  const originalFn = obj[methodName]
 
   if (typeof originalFn !== 'function') {
-    throw new TypeError('Expected obj[methodName] to be a function');
+    throw new TypeError('Expected obj[methodName] to be a function')
   }
 
   // if originalFn looks like a Spied Function, assume it is one and throw an error
   if (originalFn.callCount && originalFn.reset && originalFn.restore) {
-    throw new Error('Cannot spy on a Spied Function');
+    throw new Error('Cannot spy on a Spied Function')
   }
 
   /**
@@ -52,46 +47,46 @@ function spy(obj, methodName) {
    * @return {*} - return value of fake value for args or original function
    */
   obj[methodName] = function (...args) {
-    let i;
+    let i
 
-    obj[methodName].calls.push(args);
+    obj[methodName].calls.push(args)
 
     for (i = 0; i < withArgsReturns.length; i++) {
       if (deepEqual(withArgsReturns[i].params, args)) {
         if (withArgsReturns[i].Type) {
-          throw new withArgsReturns[i].Type(withArgsReturns[i].message);
+          throw new withArgsReturns[i].Type(withArgsReturns[i].message)
         }
 
-        return withArgsReturns[i].value;
+        return withArgsReturns[i].value
       }
     }
 
     if (spyReturn.Type) {
-      throw new spyReturn.Type(spyReturn.message);
+      throw new spyReturn.Type(spyReturn.message)
     }
 
-    return spyReturn.value || originalFn(...args);
-  };
+    return spyReturn.value || originalFn(...args)
+  }
 
-  spiedFn = obj[methodName];
+  const spiedFn = obj[methodName]
 
   // attach originalFn's properties
   Object.keys(originalFn).forEach(key => {
-    spiedFn[key] = originalFn[key];
-  });
+    spiedFn[key] = originalFn[key]
+  })
 
   /**
    * Number of times obj[methodName] has been executed
    * @return {Number} - number of times called
    */
   spiedFn.callCount = function () {
-    return spiedFn.calls.length;
-  };
+    return spiedFn.calls.length
+  }
 
   /**
    * Stored parameters of each call to Spied Function
    */
-  spiedFn.calls = [];
+  spiedFn.calls = []
 
   /**
    * Determine if Spied Function was called with parameters
@@ -99,17 +94,17 @@ function spy(obj, methodName) {
    * @return {Boolean} - if Spied function was called with params
    */
   spiedFn.calledWith = function (...params) {
-    const calls = spiedFn.calls;
-    let i;
+    const calls = spiedFn.calls
+    let i
 
     for (i = 0; i < calls.length; i++) {
       if (deepEqual(calls[i], params, {strict: true})) {
-        return true;
+        return true
       }
     }
 
-    return false;
-  };
+    return false
+  }
 
   /**
    * Resets spy analytics and fake values
@@ -118,10 +113,10 @@ function spy(obj, methodName) {
    *  - withArgsReturns = []
    */
   spiedFn.reset = function () {
-    spiedFn.calls = [];
-    spyReturn = {};
-    withArgsReturns = [];
-  };
+    spiedFn.calls = []
+    spyReturn = {}
+    withArgsReturns = []
+  }
 
   /**
    * Transforms Spied Function back to original function
@@ -129,11 +124,11 @@ function spy(obj, methodName) {
    */
   spiedFn.restore = function () {
     if (isNewSpy) {
-      throw new Error('Cannot restore a new spy');
+      throw new Error('Cannot restore a new spy')
     }
 
-    obj[methodName] = originalFn;
-  };
+    obj[methodName] = originalFn
+  }
 
   /**
    * Set return value of Spied Function to value
@@ -141,10 +136,10 @@ function spy(obj, methodName) {
    * @return {Object} - Spied Function is returned for easy chaining
    */
   spiedFn.returns = function (value) {
-    spyReturn.value = value;
+    spyReturn.value = value
 
-    return spiedFn;
-  };
+    return spiedFn
+  }
 
   /**
    * Set error to be thrown when Spied Function is called
@@ -153,11 +148,11 @@ function spy(obj, methodName) {
    * @return {Object} - Spied Function is returned for easy chaining
    */
   spiedFn.throws = function (errorType, message) {
-    spyReturn.Type = errorType;
-    spyReturn.message = message;
+    spyReturn.Type = errorType
+    spyReturn.message = message
 
-    return spiedFn;
-  };
+    return spiedFn
+  }
 
   /**
    * Set up spy analytics and returns for Spied Function with specific calls with args
@@ -173,7 +168,7 @@ function spy(obj, methodName) {
       callCount() {
         return spiedFn.calls.reduce(
           (acc, call) => deepEqual(call, params) ? acc + 1 : acc, 0
-        );
+        )
       },
 
       /**
@@ -183,20 +178,20 @@ function spy(obj, methodName) {
        * @return {Object} - Spied Function is returned for easy chaining
        */
       returns(value) {
-        let found = false;
+        let found = false
 
         withArgsReturns.forEach(withArgs => {
           if (deepEqual(withArgs.params, params)) {
-            withArgs.value = value;
-            found = true;
+            withArgs.value = value
+            found = true
           }
-        });
+        })
 
         if (!found) {
-          withArgsReturns.push({params, value});
+          withArgsReturns.push({params, value})
         }
 
-        return spiedFn;
+        return spiedFn
       },
 
       /**
@@ -206,24 +201,28 @@ function spy(obj, methodName) {
        * @return {Object} - Spied Function is returned for easy chaining
        */
       throws(errorType, message) {
-        let found = false;
+        let found = false
 
         withArgsReturns.forEach(withArgs => {
           if (deepEqual(withArgs.params, params)) {
-            withArgs.Type = errorType;
-            withArgs.message = message;
-            found = true;
+            withArgs.Type = errorType
+            withArgs.message = message
+            found = true
           }
-        });
+        })
 
         if (!found) {
-          withArgsReturns.push({params, Type: errorType, message});
+          withArgsReturns.push({params, Type: errorType, message})
         }
 
-        return spiedFn;
+        return spiedFn
       }
-    };
-  };
+    }
+  }
 
-  return spiedFn;
+  return spiedFn
+}
+
+module.exports = {
+  spy
 }
--- test/test.js
+++ test/test.js
@@ -1,333 +1,323 @@
 /* global describe, beforeEach, it */
-'use strict';
-import austin from '../lib/';
-import {expect} from 'chai';
+'use strict'
+import austin from '../lib/'
+import {expect} from 'chai'
 
 describe('austin', () => {
   describe('spy', () => {
-    let testSubject;
+    let testSubject
 
     beforeEach(() => {
       testSubject = {
         test() {}
-      };
-    });
+      }
+    })
 
     it('should throw TypeError if obj is null or undefined', () => {
-      function nullTest() {
-        austin.spy(null);
-      }
+      const nullTest = () => austin.spy(null)
 
-      function undefinedTest() {
-        austin.spy(undefined);
-      }
+      const undefinedTest = () => austin.spy(undefined)
 
-      expect(nullTest).to.throw(TypeError, /Expected obj to not be null or undefined/);
-      expect(undefinedTest).to.throw(TypeError, /Expected obj to not be null or undefined/);
-    });
+      expect(nullTest).to.throw(TypeError, /Expected obj to not be null or undefined/)
+      expect(undefinedTest).to.throw(TypeError, /Expected obj to not be null or undefined/)
+    })
 
     it('should throw TypeError if obj[methodName] is not a function', () => {
-      function test() {
-        testSubject.id = '1';
+      const test = () => {
+        testSubject.id = '1'
 
-        austin.spy(testSubject, 'id');
+        austin.spy(testSubject, 'id')
       }
 
-      expect(test).to.throw(TypeError, /Expected obj\[methodName\] to be a function/);
-    });
+      expect(test).to.throw(TypeError, /Expected obj\[methodName\] to be a function/)
+    })
 
     it('should throw Error if spy is called on a Spied Function', () => {
-      function test() {
-        austin.spy(testSubject, 'test');
-      }
+      const test = () => austin.spy(testSubject, 'test')
 
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(test).to.throw(Error, /Cannot spy on a Spied Function/);
-    });
+      expect(test).to.throw(Error, /Cannot spy on a Spied Function/)
+    })
 
     it('should add callCount function with initial value of 0 to obj[methodName]', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(testSubject.test.callCount()).to.eql(0);
-    });
+      expect(testSubject.test.callCount()).to.eql(0)
+    })
 
     it('should call original function and increment callCount when spied function is executed', () => {
       testSubject.test = function () {
-        return 5;
-      };
+        return 5
+      }
 
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(testSubject.test()).to.eql(5);
-      expect(testSubject.test.callCount()).to.eql(1);
+      expect(testSubject.test()).to.eql(5)
+      expect(testSubject.test.callCount()).to.eql(1)
 
-      testSubject.test();
-      expect(testSubject.test.callCount()).to.eql(2);
-    });
+      testSubject.test()
+      expect(testSubject.test.callCount()).to.eql(2)
+    })
 
     it('should pass params to original function', () => {
       testSubject.test = function (x, y) {
-        return x + y;
-      };
+        return x + y
+      }
 
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(testSubject.test(1, 3)).to.eql(4);
-    });
+      expect(testSubject.test(1, 3)).to.eql(4)
+    })
 
     it('should reset spy analytics and fake values when reset is called', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
       testSubject.test.returns(7)
-        .withArgs('austin').returns('powers');
+        .withArgs('austin').returns('powers')
 
-      testSubject.test();
-      testSubject.test.reset();
+      testSubject.test()
+      testSubject.test.reset()
 
-      expect(testSubject.test.callCount()).to.eql(0);
-      expect(testSubject.test.calls).to.eql([]);
-      expect(testSubject.test()).to.eql(undefined);
-      expect(testSubject.test('austin')).to.eql(undefined);
-    });
+      expect(testSubject.test.callCount()).to.eql(0)
+      expect(testSubject.test.calls).to.eql([])
+      expect(testSubject.test()).to.eql(undefined)
+      expect(testSubject.test('austin')).to.eql(undefined)
+    })
 
     it('should add restore function to obj[methodName]', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(typeof testSubject.test.restore).to.eql('function');
-    });
+      expect(typeof testSubject.test.restore).to.eql('function')
+    })
 
     it('should add empty calls list to Spied Function', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(testSubject.test.calls).to.eql([]);
-    });
+      expect(testSubject.test.calls).to.eql([])
+    })
 
     it('should store parameters passed to Spied Function', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      testSubject.test('hi');
+      testSubject.test('hi')
       testSubject.test(null, 1, {
         x: {
           y: 'hi'
         }
-      });
-      testSubject.test();
+      })
+      testSubject.test()
 
-      expect(testSubject.test.calls[0]).to.eql(['hi']);
+      expect(testSubject.test.calls[0]).to.eql(['hi'])
       expect(testSubject.test.calls[1]).to.eql([null, 1, {
         x: {
           y: 'hi'
         }
-      }]);
-      expect(testSubject.test.calls[2]).to.eql([]);
-    });
+      }])
+      expect(testSubject.test.calls[2]).to.eql([])
+    })
 
     it('should add calledWith function to Spied Function', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(typeof testSubject.test.calledWith).to.eql('function');
-    });
+      expect(typeof testSubject.test.calledWith).to.eql('function')
+    })
 
     it('should determine if Spied Function was called with args', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      testSubject.test('hi');
-      testSubject.test(null, 1, undefined, ['cat']);
-      testSubject.test();
+      testSubject.test('hi')
+      testSubject.test(null, 1, undefined, ['cat'])
+      testSubject.test()
 
-      expect(testSubject.test.calledWith('hi')).to.eql(true);
-      expect(testSubject.test.calledWith(null, 1, undefined, ['cat'])).to.eql(true);
-      expect(testSubject.test.calledWith()).to.eql(true);
+      expect(testSubject.test.calledWith('hi')).to.eql(true)
+      expect(testSubject.test.calledWith(null, 1, undefined, ['cat'])).to.eql(true)
+      expect(testSubject.test.calledWith()).to.eql(true)
 
-      expect(testSubject.test.calledWith('bye')).to.eql(false);
-    });
+      expect(testSubject.test.calledWith('bye')).to.eql(false)
+    })
 
     it('should add returns function to Spied Function', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(typeof testSubject.test.returns).to.eql('function');
-    });
+      expect(typeof testSubject.test.returns).to.eql('function')
+    })
 
     it('should return returns value when Spied Function is called', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      testSubject.test.returns(7);
+      testSubject.test.returns(7)
 
-      expect(testSubject.test()).to.eql(7);
-    });
+      expect(testSubject.test()).to.eql(7)
+    })
 
     it('should override returns value when called again', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      testSubject.test.returns(7);
-      testSubject.test.returns(5);
+      testSubject.test.returns(7)
+      testSubject.test.returns(5)
 
-      expect(testSubject.test()).to.eql(5);
-    });
+      expect(testSubject.test()).to.eql(5)
+    })
 
     it('should throw error when Spied Function is called', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      testSubject.test.throws(Error);
-      expect(testSubject.test).to.throw(Error);
+      testSubject.test.throws(Error)
+      expect(testSubject.test).to.throw(Error)
 
-      testSubject.test.throws(TypeError);
-      expect(testSubject.test).to.throw(TypeError);
-    });
+      testSubject.test.throws(TypeError)
+      expect(testSubject.test).to.throw(TypeError)
+    })
 
     it('should throw error with message when Spied Function is called', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      testSubject.test.throws(TypeError, 'Expected value to be a string');
+      testSubject.test.throws(TypeError, 'Expected value to be a string')
 
-      expect(testSubject.test).to.throw(TypeError, /Expected value to be a string/);
-    });
+      expect(testSubject.test).to.throw(TypeError, /Expected value to be a string/)
+    })
 
     it('should return Spied Function when throws is called', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(testSubject.test.throws(Error)).to.eql(testSubject.test);
-    });
+      expect(testSubject.test.throws(Error)).to.eql(testSubject.test)
+    })
 
     it('should add withArgs function to Spied Function', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(typeof testSubject.test.withArgs).to.eql('function');
-    });
+      expect(typeof testSubject.test.withArgs).to.eql('function')
+    })
 
     it('should return object with returns on withArgs call', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      expect(typeof testSubject.test.withArgs().returns).to.eql('function');
-    });
+      expect(typeof testSubject.test.withArgs().returns).to.eql('function')
+    })
 
     it('should return mock value for withArgs->returns setup', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      testSubject.test.withArgs('hi').returns('bye');
-      testSubject.test.withArgs('austin').returns('powers');
+      testSubject.test.withArgs('hi').returns('bye')
+      testSubject.test.withArgs('austin').returns('powers')
 
-      expect(testSubject.test('hi')).to.eql('bye');
-      expect(testSubject.test('austin')).to.eql('powers');
-      expect(testSubject.test()).to.eql(undefined);
-    });
+      expect(testSubject.test('hi')).to.eql('bye')
+      expect(testSubject.test('austin')).to.eql('powers')
+      expect(testSubject.test()).to.eql(undefined)
+    })
 
     it('should throw error for withArgs->throws setup', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
       testSubject.test
         .withArgs('hi').throws(TypeError)
         .withArgs('hi').throws(SyntaxError)
-        .withArgs('austin').throws(TypeError, 'Expected value to be a string');
+        .withArgs('austin').throws(TypeError, 'Expected value to be a string')
 
-      function hiTest() {
-        testSubject.test('hi');
-      }
+      const hiTest = () => testSubject.test('hi')
 
-      function austinTest() {
-        testSubject.test('austin');
-      }
+      const austinTest = () => testSubject.test('austin')
 
-      expect(hiTest).to.throw(SyntaxError);
-      expect(austinTest).to.throw(TypeError, 'Expected value to be a string');
-    });
+      expect(hiTest).to.throw(SyntaxError)
+      expect(austinTest).to.throw(TypeError, 'Expected value to be a string')
+    })
 
     it('should override withArgs->returns value when called more than once', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      testSubject.test.withArgs('hello').returns('bye');
-      testSubject.test.withArgs('hello').returns('goodbye');
+      testSubject.test.withArgs('hello').returns('bye')
+      testSubject.test.withArgs('hello').returns('goodbye')
 
-      expect(testSubject.test('hello')).to.eql('goodbye');
-    });
+      expect(testSubject.test('hello')).to.eql('goodbye')
+    })
 
     it('should return Spied Function after setting withArgs->returns', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
       testSubject.test
         .withArgs('hi').returns('bye')
-        .withArgs('austin').returns('powers');
+        .withArgs('austin').returns('powers')
 
-      expect(testSubject.test('hi')).to.eql('bye');
-      expect(testSubject.test('austin')).to.eql('powers');
-      expect(testSubject.test()).to.eql(undefined);
-      expect(testSubject.test('dog')).to.eql(undefined);
-    });
+      expect(testSubject.test('hi')).to.eql('bye')
+      expect(testSubject.test('austin')).to.eql('powers')
+      expect(testSubject.test()).to.eql(undefined)
+      expect(testSubject.test('dog')).to.eql(undefined)
+    })
 
     it('should return Spied Function when returns is called', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
       testSubject.test.returns('bye')
-        .withArgs('austin').returns('powers');
+        .withArgs('austin').returns('powers')
 
-      expect(testSubject.test()).to.eql('bye');
-      expect(testSubject.test('austin')).to.eql('powers');
-      expect(testSubject.test('dog')).to.eql('bye');
-    });
+      expect(testSubject.test()).to.eql('bye')
+      expect(testSubject.test('austin')).to.eql('powers')
+      expect(testSubject.test('dog')).to.eql('bye')
+    })
 
     it('should return callCount function on withArgs', () => {
-      austin.spy(testSubject, 'test');
+      austin.spy(testSubject, 'test')
 
-      testSubject.test('cat');
-      testSubject.test('dog');
-      testSubject.test('dog');
+      testSubject.test('cat')
+      testSubject.test('dog')
+      testSubject.test('dog')
 
-      expect(testSubject.test.withArgs().callCount()).to.eql(0);
-      expect(testSubject.test.withArgs('cat').callCount()).to.eql(1);
-      expect(testSubject.test.withArgs('dog').callCount()).to.eql(2);
-      expect(testSubject.test.callCount()).to.eql(3);
-    });
+      expect(testSubject.test.withArgs().callCount()).to.eql(0)
+      expect(testSubject.test.withArgs('cat').callCount()).to.eql(1)
+      expect(testSubject.test.withArgs('dog').callCount()).to.eql(2)
+      expect(testSubject.test.callCount()).to.eql(3)
+    })
 
     it('should retain original function\'s properties', () => {
-      testSubject.fancy = function () {};
+      testSubject.fancy = function () {}
       testSubject.fancy.hello = function () {
-        return 'hi';
-      };
+        return 'hi'
+      }
 
-      austin.spy(testSubject, 'fancy');
+      austin.spy(testSubject, 'fancy')
 
-      expect(testSubject.fancy.hello()).to.eql('hi');
-    });
+      expect(testSubject.fancy.hello()).to.eql('hi')
+    })
 
     it('should return Spied Function on initial spy for easy chaining', () => {
-      austin.spy(testSubject, 'test').returns('dog');
+      austin.spy(testSubject, 'test').returns('dog')
 
-      expect(testSubject.test()).to.eql('dog');
-    });
+      expect(testSubject.test()).to.eql('dog')
+    })
 
     it('should restore spied function to original function', () => {
       testSubject.test = function () {
-        return 1;
-      };
+        return 1
+      }
 
-      austin.spy(testSubject, 'test');
-      testSubject.test.returns(7);
-      testSubject.test.restore();
+      austin.spy(testSubject, 'test')
+      testSubject.test.returns(7)
+      testSubject.test.restore()
 
-      expect(testSubject.test()).to.eql(1);
-      expect(testSubject.test.calls).to.eql(undefined);
-      expect(testSubject.test.callCount).to.eql(undefined);
-      expect(testSubject.test.restore).to.eql(undefined);
-      expect(testSubject.test.returns).to.eql(undefined);
-      expect(testSubject.test.withArgs).to.eql(undefined);
-    });
+      expect(testSubject.test()).to.eql(1)
+      expect(testSubject.test.calls).to.eql(undefined)
+      expect(testSubject.test.callCount).to.eql(undefined)
+      expect(testSubject.test.restore).to.eql(undefined)
+      expect(testSubject.test.returns).to.eql(undefined)
+      expect(testSubject.test.withArgs).to.eql(undefined)
+    })
 
     describe('new spy', () => {
-      let x;
+      let x
 
       beforeEach(() => {
-        x = austin.spy();
-      });
+        x = austin.spy()
+      })
 
       it('should be able to set a value to a spy', () => {
-        x.returns(7);
+        x.returns(7)
 
-        expect(x()).to.eql(7);
-        expect(x.callCount()).to.eql(1);
-      });
+        expect(x()).to.eql(7)
+        expect(x.callCount()).to.eql(1)
+      })
 
       it('should throw error if trying to restore a new spy', () => {
-        expect(x.restore).to.throw(Error, 'Cannot restore a new spy');
-      });
-    });
-  });
-});
+        expect(x.restore).to.throw(Error, 'Cannot restore a new spy')
+      })
+    })
+  })
+})
